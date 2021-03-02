c27a7ffd8d5a01db59e6df57effd9628a751e408
Dale Foskey <developer.dale.foskey>
2021-03-01 22:21:22 -0500

    created the logic for php file and updated background picure

--- Foskey-A7-ticket-order.php
+++ Foskey-A7-ticket-order.php
@@ -22,7 +22,7 @@
 			
 			Algorithm for Foskey-A7-ticket-order.php
 			    Receive numTickets, artist, concertNight from Foskey-A7-ticket-order.html
-                If (artist == Beasty Boys)
+                If (artist == Madonna)
                     ticketCost = 75.00
                 ElseIf (artist == Metallica)
                     ticketCost = 125.00
@@ -37,9 +37,36 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel ="stylesheet" type="text/css" href="Foskey-A7-ticket-order.css">
+    <title>Ticket Sales Completed</title>
 </head>
 <body>
-    
+    <h2 class="header">Thank you for your purchase</h2>
+    <?php
+    // assign the variables to the array information from the html file
+    $numTickets = $_POST['numTickets'];
+    $artist = $_POST['artist'];
+    $concertNight = $_POST['concertNight'];
+    // if else statement to select which artist and price to choose
+    if ($artist == "Modonna")
+    {
+        $ticketCost = 75.00;
+    }
+    else 
+    {
+        $ticketCost = 125.00;
+    }
+    // calculating the total cost
+    $totalCost = $ticketCost * $numTickets;
+
+    print("You have ordered $numTickets tickets to see $artist in concert on $concertNight and your total cost is $$totalCost ");
+    // another if else statement to show the friday disclamer
+    if ($concertNight == "Friday")
+    {
+        print("<br>Since you are attending on a Friday night, expect heavy traffic.");
+    }
+?>
+    <br>
+    <a href="Foskey-A7-ticket-order.html"> Return to Form</a>
 </body>
 </html>
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # ticketApp
- 
+
+## An app to sell tickets and use some php logic to calculate
+
+## created by: Dale Foskey
