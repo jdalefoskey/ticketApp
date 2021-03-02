451c28758f9b0e6566a393cc358b7cc514624071
Dale Foskey <developer.dale.foskey>
2021-03-01 19:42:02 -0500

    Created IPO and algorithm

---
+++ Foskey-A7-ticket-order.php
@@ -0,0 +1,45 @@
+-<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!--	Author: Dale Foskey
+			Date:   03/01/21
+			File:	Foskey-A7-ticket-order.php
+			Purpose: Assignment 7
+			
+			IPO:
+			   Inputs: number of tickets, artist, concert night
+			   Processing: receive number of tickets, artist, concert night
+			               Caculate number of tickets
+                           calculate ticket cost 
+                           calculate total cost
+			               Display artist
+                           Display ticket cost
+                           Display concert night
+                           Display total cost
+                           Display friday message
+			               
+			   Outputs: artist, ticket cost, concert night, total cost, friday message
+			
+			Algorithm for Foskey-A7-ticket-order.php
+			    Receive numTickets, artist, concertNight from Foskey-A7-ticket-order.html
+                If (artist == Beasty Boys)
+                    ticketCost = 75.00
+                ElseIf (artist == Metallica)
+                    ticketCost = 125.00
+                EndIf
+                totalCost = numTickets * ticketCost
+                Display artist,ticketCost,concertNight, totalCost
+                If (concertNight == friday)
+                    Display <p>Since you are attending on Friday night, expect heavy traffic</p>
+                close  
+                END 
+	-->
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
